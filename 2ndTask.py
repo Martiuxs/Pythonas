6ff6a1913f5b60de4f9266f553c7ca1921a019a5
Martiuxs <kompoprofas@gmail.com>
2023-07-03 15:11:31 +0300

    Workin

--- 2ndTask.py
+++ 2ndTask.py
@@ -2,6 +2,7 @@ import json
 import paramiko
 import default_config
 import requests
+import socket
 # Load the configuration from JSON file
 with open('config.json') as f:
     config = json.load(f)
@@ -18,29 +19,86 @@ def authenticate(router_ip, username, password):
         print("Authentication failed.")
         return None
     
+def assign(token):
+    url = "http://192.168.1.1/api/bulk"
+
+    payload = json.dumps({
+    "data": [
+        {
+        "method": "PUT",
+        "endpoint": "/api/network/wireless/devices/config/radio0/interfaces",
+        "data": [
+            {
+            "enabled": "1",
+            ".type": "wifi-iface",
+            "id": "default_radio0"
+            },
+        ],
+        "awaitNetwork": True
+        }
+    ]
+    })
+    headers = {
+    'Authorization': f"Bearer {token}",
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+   
 # SSH connection details
 router_ip = default_config.router_ip
 port = int(default_config.port)
 username = 'root'
 password = default_config.password
 
+config_lines = [
+     "",
+ "config wifi-iface",
+    "\toption encryption 'none'",
+    "\toption device 'radio0'",
+    "\toption mode 'ap'",
+    "\toption wifi_id 'wifi1'",
+    "\toption skip_inactivity_poll '0'",
+    "\toption network 'lan'",
+    "\toption hidden '0'",
+    "\toption ieee80211r '0'",
+    "\toption isolate '0'",
+    "\toption ssid 'Testas123456'",
+    "\toption short_preamble '1'",
+    "\toption disassoc_low_ack '1'",
+    "\toption short_preamble 1",
+    "\toption ieee80211r 0",
+    "\toption encryption none",
+    "\toption wifi_id wifi2"
+]
 
-
-# SSH connection setup
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname=router_ip, port=port, username=username, password=password)
 
-# Add the new access point to the configuration file
-# command = f"echo 'ssid={ssid}' >> /path/to/config/file"
-# stdin, stdout, stderr = ssh.exec_command(command)
+try:
+    ssh.connect(hostname=router_ip, port=port, username=username, password=password)
+    print("SSH connection successful!")
 
-# Verify the changes in the web UI (you may need to adapt this step to your specific router)
-# web_ui_command = "command_to_verify_changes_in_web_ui"
-# stdin, stdout, stderr = ssh.exec_command(web_ui_command)
+     # Read existing content of the wireless file
+    command = "cat /etc/config/wireless"
+    stdin, stdout, stderr = ssh.exec_command(command)
+    existing_content = stdout.read().decode()
 
-# Print the output of the web UI verification
-# print(stdout.read().decode())
+    # Modify the existing content by adding the new configuration lines
+    modified_content = existing_content.strip() + '\n' + '\n'.join(config_lines)
 
-# Close the SSH connection
-ssh.close()
+    # Write the modified content back to the wireless file
+    command = f"echo -e '{modified_content}' > /etc/config/wireless"
+    stdin, stdout, stderr = ssh.exec_command(command)
+    print("Configuration lines added to /etc/config/wireless file.")
+    
+except paramiko.AuthenticationException:
+    print("Authentication failed. Please check your credentials.")
+except paramiko.SSHException as ssh_exception:
+    print(f"SSH connection error: {str(ssh_exception)}")
+except Exception as e:
+    print(f"Error: {str(e)}")
+finally:
+  
+    ssh.close()
